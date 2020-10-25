9375349af8bffe75c241fc9e8d7e6462e1ea7503
EarthenLynx <t.quante@gmx.net>
2020-10-25 16:52:13 +0100

    Added trycatch for user data

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,9 +4,17 @@ const User = new UserModel(DB_HOST, DB_USERNAME, DB_USER_PASSWORD, DB_DATABASE_N
 
 const handleGetMyData = async (req, res, session) => {
   const connection = await User.connect()
-  const user = await User.getMyData(connection, session).catch(err => err)
-  res.status(200).send({ status: 'success', msg: 'Fetched your user data', data: user });
-  return User.close(connection)
+
+  try {
+    const user = await User.getMyData(connection, session).catch(err => err)
+    res.status(200).send({ status: 'success', msg: 'Fetched your user data', data: user });
+  } catch (error) {
+    res.status(500).send({ status: 'error', msg: err.message })
+  } finally {
+    return User.close(connection)
+
+  }
+
 }
 
 const handleUpdateMyData = async (req, res, session) => {
@@ -17,7 +25,6 @@ const handleUpdateMyData = async (req, res, session) => {
     const success = await User.updateMyData(connection, session, payload)
     res.status(201).send({ status: 'success', msg: 'Your userdata have been updated' });
   } catch (err) {
-    console.log(err);
     res.status(500).send({ status: 'error', msg: err.message })
   } finally {
     return User.close(connection)
